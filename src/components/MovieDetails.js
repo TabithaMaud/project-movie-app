9f02255fa83712d71dcf846d698661447f5e29ac
TabithaMaud <tabitha.m.perry@gmail.com>
2021-06-06 15:24:39 -0400

    add local storage and favorite page for users to save movies to watch in the future. Complete add functionality and render of movies

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,27 +1,51 @@
-import React, { Component } from 'react';
-import noImage from '../images/no-image.jpeg';
+import React, { useState, useEffect } from "react";
+import noImage from "../images/no-image.jpeg";
 
-class MovieDetails extends Component {
-	render() {
-		return (
-			<section className='movieDetails'>
-				<img
-					src={
-						this.props.poster
-							? this.props.imageUrl + this.props.poster
-							: noImage
-					}
-					alt={this.props.title}
-					key={this.props.id}
-				/>
-				<div className='movieOverview'>
-					<h1>{this.props.title}</h1>
-					<p>{this.props.date}</p>
-					<div>{this.props.overview}</div>
-				</div>
-			</section>
-		);
-	}
+function MovieDetails({ movie, imageUrl, favorites, getFavorites }) {
+  useEffect(() => {
+    if (localStorage.getItem("myFavMovies")) {
+      const data = JSON.parse(localStorage.getItem("myFavMovies"));
+      getFavorites(data);
+    }
+  }, []);
+
+  const handleClick = (event) => {
+    if (!localStorage.getItem("myFavMovies")) {
+      getFavorites([movie]);
+      localStorage.setItem("myFavMovies", JSON.stringify([movie]));
+    } else {
+      let movieFound = false;
+      let favs = favorites;
+      favs.forEach((fav) => {
+        if (fav.id === movie.id) {
+          movieFound = true;
+        }
+      });
+      if (!movieFound) {
+        favs.push(movie);
+        getFavorites(favs);
+        localStorage.setItem("myFavMovies", JSON.stringify(favorites));
+      }
+    }
+  };
+
+  return (
+    <section className="movieDetails">
+      <img
+        src={movie.poster_path ? imageUrl + movie.poster_path : noImage}
+        alt={movie.title}
+        key={movie.id}
+      />
+      <div className="movieOverview">
+        <h1>{movie.title}</h1>
+        <p>{movie.date}</p>
+        <div>{movie.overview}</div>
+        <button id={movie.id} onClick={handleClick}>
+          Add to favs
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default MovieDetails;
